7d1f59156c092749e8bcf2f9bef55c963e482ab4
Nick Angelou <angelou.nick@gmail.com>
2025-10-22 00:15:11 -0500

    fix

--- crates/ferroid/src/generator/mutex.rs
+++ crates/ferroid/src/generator/mutex.rs
@@ -1,9 +1,5 @@
 #[cfg(all(feature = "lock", not(feature = "parking-lot")))]
-pub type Mutex = std::sync::Mutex;
-#[cfg(all(feature = "lock", not(feature = "parking-lot")))]
-pub type MutexGuard = std::sync::MutexGuard;
-#[cfg(all(feature = "lock", not(feature = "parking-lot")))]
-pub type PoisonError = std::sync::PoisonError;
+pub use std::sync::{Mutex, MutexGuard, PoisonError};
 
 #[cfg(feature = "parking-lot")]
 pub use parking_lot::{Mutex, MutexGuard};
